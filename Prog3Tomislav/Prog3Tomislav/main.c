292d223f6d94ff8c502a6941f5e3857f9c576508
Francezi <tomislavfrancezi@gmail.com>
2025-06-17 11:33:44 +0200

    main.c

--- Prog3Tomislav/Prog3Tomislav/main.c
+++ Prog3Tomislav/Prog3Tomislav/main.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "funkcije.h"
 
 int main() {
     int izbor;
 
-    while (1) {
-        printf("\n--- Glavni izbornik ---\n");
+    do {
+        printf("\n--- Porsche Modeli ---\n");
         printf("1. Unesi novi model\n");
         printf("2. Prikazi modele\n");
         printf("3. Uredi model\n");
         printf("4. Izbrisi model\n");
         printf("5. Izlaz\n");
-
-        izbor = unosBroja("Izbor: ");
+        printf("Izbor: ");
+        scanf("%d", &izbor);
+        getchar(); // da bi pročitao novi red nakon unosa
 
         switch (izbor) {
-            case 1:
-                unesiModel();
-                break;
-            case 2:
-                prikaziModele();
-                break;
-            case 3:
-                urediModel();
-                break;
-            case 4:
-                izbrisiModel();
-                break;
-            case 5:
-                printf("Izlaz...\n");
-                return 0;
-            default:
-                printf("Nepoznata opcija, pokusajte ponovo.\n");
+        case 1:
+            unesiModel();
+            break;
+        case 2:
+            prikaziModele();
+            break;
+        case 3:
+            urediModel();
+            break;
+        case 4:
+            izbrisiModel();
+            break;
+        case 5:
+            printf("Izlaz...\n");
+            break;
+        default:
+            printf("Nepoznata opcija. Pokusajte ponovo.\n");
         }
-    }
+    } while (izbor != 5);
 
     return 0;
 }
